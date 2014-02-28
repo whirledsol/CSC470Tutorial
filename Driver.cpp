ae25d09df90e68c86bc380a2bed4b8f491fc0b15
Will Rhodes <astro491@gmail.com>
2014-02-28 01:28:03 -0500

    Added driver stuff

--- Driver.cpp
+++ Driver.cpp
@@ -14,15 +14,41 @@
 #include <cctype>
 #include <string>
 #include "Site.h"
-
+#include "SiteList.h"
 using namespace std;
 
-
+SiteList parseFile(string filename);
 
 int main(){
+	cout<<"WHAT"<endl;
+	string filename;
+	SiteList mySites;
 	
-	Site test;
-    test = Site(5,"","a","b","c",5,6);
-	cout<<test.toString()<<endl;
+	cout<<"What file would like to input?"<<endl;
+	cin>>filename;
+	mySites = parseFile(filename);
+	
+	
 	return 0;
+}
+
+SiteList parseFile(string filename){
+
+	ifstream fin;
+	SiteList mySites;
+	
+	fin.open(filename.c_str());
+	if (fin.fail()){
+		cout<<"The file couldn't open. Try again..."<<endl;
+		main();
+	}
+	else{
+		//we opened the file
+		string test;
+		getline(fin,test,',');
+		
+		cout<<"SURVEY SAYS X"<<test<<"X"<<endl;
+	}
+	
+	return mySites;
 }
